4b48d3f2f88e0df573bc479ecef7c6f53ebdc68b
Igorprogramacao <igorinacio02@gmail.com>
2025-03-03 17:14:36 +0000

    cartas super trunfo de países

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,53 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char estado;
+    char codigo [20];
+    char cidade[50];
+    int populacao;
+    float area;
+    float pib;
+    int turismo;
+    //pib(produto interno bruto)
+
+    printf("Carta 1 \n");
+
+    printf("Estado: \n");
+    scanf("%c", &estado);
+
+    printf("Codigo da carta: \n");
+    scanf("%s", codigo);
+
+    printf("Nome da cidade: \n");
+    scanf("%s", cidade);
+
+    printf("População: \n");
+    scanf("%d", &populacao);
+
+    printf("Área KM²: \n");
+    scanf("%f", &area);
+
+    printf("PIB: \n");
+    scanf("%f", &pib);
+
+    printf("Número de pontos turísticos: \n");
+    scanf("%d", &turismo);
+
+    printf("Estado: %c\n", estado);
+    printf("Codigo: %s\n", codigo);
+    printf("Cidade: %s\n", cidade);
+    printf("Área KM² %f\n", area);
+    printf("PIB: %f\n", pib);
+    printf("Número de pontos turísticos %d\n", turismo);
+
+    /*
+    %d: Imprime um inteiro no formato decimal.
+    %i: Equivalente a %d
+    %f: Imprime um número de ponto flutuante no formato padrao
+    %e: Imprime um numero de ponto flutuante na notação científica
+    %c: Imprime um uniuco caractere
+    %s: Imprime uma cadeira (strings) de caracteres
+    */
 
     return 0;
 }
